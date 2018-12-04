1483e42b75c22742713f6a5da6ae5e39beb6d01d
Ognjen Vukovic <ognjen.m.vukovic@gmail.com>
2018-12-05 00:32:07 +0100

    Rename app players to tournament

--- src/tgifoosball/urls.py
+++ src/tgifoosball/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from players import views
+from tournament import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
