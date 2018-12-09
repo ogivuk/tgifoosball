bc8d9d2ecc41cba053b1a05b56055ef07d9efd92
Ognjen Vukovic <ognjen.m.vukovic@gmail.com>
2018-12-09 16:58:42 +0100

    Model for list Players and associated migrations

--- src/tournament/tests.py
+++ src/tournament/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from tournament.views import home_page
+from tournament.models import Player
 
 class HomePageTest(TestCase):
 
@@ -19,3 +20,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new player'})
         self.assertIn('A new player', response.content.decode())
         self.assertTemplateUsed(response,'home.html')
+
+class PlayerModelTest(TestCase):
+
+    def test_saving_and_retrieving_players(self):
+        first_player = Player()
+        first_player.name = "The first (ever) player"
+        first_player.save()
+
+        second_player = Player()
+        second_player.name = "Player the second"
+        second_player.save()
+
+        saved_players = Player.objects.all()
+        self.assertEqual(saved_players.count(), 2)
+
+        first_saved_player = saved_players[0]
+        second_saved_player = saved_players[1]
+        self.assertEqual(first_saved_player.name, "The first (ever) player")
+        self.assertEqual(second_saved_player.name, "Player the second")
