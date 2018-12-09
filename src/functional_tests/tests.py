d636e9296f6892cd2a7e778690390bae5d5ee32c
Ognjen Vukovic <ognjen.m.vukovic@gmail.com>
2018-12-09 19:42:08 +0100

    Replace explicit waits with try/except loop

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text,[row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)      
 
     def test_can_add_players_and_retrieve_them_later(self):
         # Foosball local admin opens the web app
@@ -38,19 +49,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When the admin hits enter, the page updates, and now the page lists
         # "1: John Doe" as a player in the list of players:
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: John Doe')
+        self.wait_for_row_in_list_table('1: John Doe')
 
         # There is still a text box inviting the admin to add another player.
         # The admin enters "Jenny Doe":
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Jenny Doe')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both players in the list:
-        self.check_for_row_in_list_table('1: John Doe')
-        self.check_for_row_in_list_table('2: Jenny Doe')
+        self.wait_for_row_in_list_table('1: John Doe')
+        self.wait_for_row_in_list_table('2: Jenny Doe')
 
         self.fail('Finish the test!')
 
